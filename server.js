7d557179eb70686b9f1c778f32be85c8ec951555
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-20 10:21:24 +0200

    Create basic server skeleton

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8000;
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running...");
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: "Not found..." });
+});
