212c8adf4e366cf0b616fb7b67b13c665d6b3ad9
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-20 14:06:23 +0200

    Add cors origin path

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const socket = require("socket.io");
 const { tasks } = require("./db/db");
 
 const app = express();
-const PORT = 8000;
+app.use(cors());
+const PORT = process.env.PORT || 8000;
 
-const server = app.listen(process.env.PORT || PORT, () => {
-  console.log("Server is running...");
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
-
-const io = socket(server);
 
 app.use((req, res) => {
   res.status(404).send({ message: "Not found..." });
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
 io.on("connection", (socket) => {
