855a670c8fd40fbe6112908b2ce66d74e9435abf
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-20 11:27:57 +0200

    Create server listener on connection event

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const socket = require("socket.io");
+const { tasks } = require("./db/db");
 
 const app = express();
 const PORT = 8000;
 
-const server = app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || PORT, () => {
   console.log("Server is running...");
 });
 
@@ -12,4 +13,8 @@ const io = socket(server);
 
 app.use((req, res) => {
   res.status(404).send({ message: "Not found..." });
+});
+
+io.on("connection", (socket) => {
+  console.log("New client! Its id – " + socket.id);
 });
