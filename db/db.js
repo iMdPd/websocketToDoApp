402d89db5f646314936a160f6fd6f22952ba6ab7
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-20 11:05:52 +0200

    Add db tasks array

---
+++ db/db.js
@@ -0,0 +1,11 @@
+const db = {
+  tasks: [
+    { id: 1, name: "Shopping" },
+    { id: 2, name: "Training" },
+    { id: 3, name: "Walk the dog" },
+    { id: 4, name: "Office meeting" },
+    { id: 5, name: "Running" },
+  ],
+};
+
+module.exports = db;
